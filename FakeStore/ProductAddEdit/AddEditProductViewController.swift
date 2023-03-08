ea2e9d6397d1fcc7f33ff3afd6463c542acf89db
JCB17373 <jcb17373@gmail.com>
2023-03-08 20:56:24 +0530

    changes for add product image

--- FakeStore/ProductAddEdit/AddEditProductViewController.swift
+++ FakeStore/ProductAddEdit/AddEditProductViewController.swift
@@ -26,6 +26,38 @@ class AddEditProductViewController: UIViewController {
         }
     }
     func addEditProductImage(){
+        let alert = UIAlertController(title: "UPLOAD", message: nil, preferredStyle: .actionSheet)
+        
+        let takeAction = UIAlertAction(title: "Take a photo", style: .default, handler: take)
+        let upload = UIAlertAction(title: "Upload from library", style: .default, handler: uploadImage)
+        let CancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: cancel)
+        
+        alert.addAction(takeAction)
+        alert.addAction(upload)
+        alert.addAction(CancelAction)
+        alert.popoverPresentationController?.sourceView = self
+        alert.popoverPresentationController?.sourceRect = CGRect(x:self.bounds.size.width / 2.0, y: self.bounds.size.height / 2.0, width : 1.0, height : 1.0)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    func take(alertAction: UIAlertAction!) {
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = false
+        picker.delegate = self
+        self.viewContainingController?.present(picker, animated: true, completion: nil)
+    }
+    func uploadImage(alertAction: UIAlertAction!) {
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        picker.allowsEditing = false
+        self.present(picker, animated: true, completion: nil)
+        
+    }
+    func cancel(alertAction: UIAlertAction!) {
         
     }
     func setViews(){
@@ -49,4 +81,23 @@ class AddEditProductViewController: UIViewController {
     @IBAction func saveClicked(_ sender: UIButton) {
     }
     
+}
+extension AddEditProductViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            print(image)
+            if let data = image.jpegCompressToMaxLimit() {
+                print(data, data.count)
+                var fileName = "image.jpeg"
+                if let url = info[UIImagePickerController.InfoKey.imageURL] as? URL {
+                    fileName = url.lastPathComponent
+                }
+
+            }
+        } else {
+            print("Something went wrong")
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
 }
